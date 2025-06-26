889736494d553aa935e5404c93680215b83bbe74
kodar@siri <shirishakodari35@.com>
2025-06-26 14:50:13 +0530

    terraform

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "main" {
   vpc_id      = var.vpc_id
 
   tags = merge(
-    ar.sg_tags, 
+    var.sg_tags, 
     local.common_tags,{
         Name = "${var.project}-${var.environment}-${var.sg_name}"
     }
