d9239ba9331176c4197b8b8f9942ed8a4f963504
kodar@siri <shirishakodari35@.com>
2025-08-13 20:01:07 +0530

    kubernets

--- main.tf
+++ main.tf
@@ -1,9 +1,20 @@
 
 #roboshop-dev-frontend
 resource "aws_security_group" "main" {
-  name        = var.sg_name
+  name        = "${var.project}-${var.environment}-${var.sg_name}"
   description = var.sg_description
   vpc_id      = var.vpc_id
+
+ 
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 
   tags = merge(
     var.sg_tags, 
